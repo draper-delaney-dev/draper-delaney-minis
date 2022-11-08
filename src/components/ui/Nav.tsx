bd208573d6b1ac447b8fd949556e80136e7a95c9
Brian D <7Delaney.Brian@gmail.com>
2022-11-08 16:51:55 -0600

    added simple routing via react-router@6

--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Nav = () => {
@@ -7,17 +8,19 @@ const Nav = () => {
   return (
     <>
       <nav className="flex sm:justify-between justify-center bg-blue-500 py-6 sm:px-24">
-        <a href="#" className="sm:flex text-4xl font-bold p-2 rounded hover:bg-blue-800">Minis</a>
+        <NavLink to="/" className="sm:flex text-4xl font-bold p-2 rounded hover:bg-blue-800">Minis</NavLink>
         <div className="lg:flex flex-1 justify-end self-center mr-16 hidden">
-          <a href="#" className="text-xl rounded hover:bg-blue-800 p-2 m-2">About</a>
-          <a href="#" className="text-xl rounded hover:bg-blue-800 p-2 m-2">Menu</a>
+          <NavLink to="/about" className="text-xl rounded hover:bg-blue-800 p-2 m-2">About</NavLink>
+          <NavLink to="/menu" className="text-xl rounded hover:bg-blue-800 p-2 m-2">Menu</NavLink>
+          <NavLink to="/contact" className="text-xl rounded hover:bg-blue-800 p-2 m-2">Contact</NavLink>
         </div>
-        <a href="#" className="lg:flex text-2xl self-center rounded hover:bg-blue-800 p-2 hidden">Order</a>
+        <NavLink to="/order" className="lg:flex text-2xl self-center rounded hover:bg-blue-800 p-2 hidden">Order</NavLink>
         <GiHamburgerMenu size="3rem" className="lg:hidden w-20 self-center align-end min-w-max rounded hover:bg-blue-800 hover:cursor-pointer" onClick={() => setNavbar(!navbar)} />
       </nav>
       <div className={`bg-blue-500 self-center lg:hidden ${navbar ? 'grid' : 'hidden'}`}>
-        <a href="#" className="text-xl hover:bg-blue-800 p-2">About</a>
-        <a href="#" className="text-xl hover:bg-blue-800 p-2">Menu</a>
+        <NavLink to="/about" className="text-xl hover:bg-blue-800 p-2">About</NavLink>
+        <NavLink to="/menu" className="text-xl hover:bg-blue-800 p-2">Menu</NavLink>
+        <NavLink to="/contact" className="text-xl hover:bg-blue-800 p-2">Contact</NavLink>
       </div>
     </>
   );
