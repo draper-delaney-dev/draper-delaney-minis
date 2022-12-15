eabc3333fa351c3a2429f322acb6be835722be36
zynsis <7delaney.brian@gmail.com>
2022-12-15 14:21:44 -0600

    add simple mock up for menu page

--- src/components/MenuItems/MenuItemCard.tsx
+++ src/components/MenuItems/MenuItemCard.tsx
@@ -1,11 +1,19 @@
 import Card from '../ui/Card';
+import foodImage from '../../assets/mini-food-1.jpg';
 
 const MenuItemCard = () => {
-    return (
-        <Card classes="">
-            
-        </Card>
-    );
+  return (
+    <Card classes="w-9/12 m-8">
+      {/* image */}
+      <img src={foodImage} alt="a plate of food" className="rounded-t"/>
+      {/* description */}
+      <p className="p-8 text-center">not sure what this is but it looked good and I just needed a placeholder</p>
+      {/* price */}
+      <div className="bg-slate-800 rounded-b">
+        <p className="p-4 text-center text-slate-100">$12.99</p>
+      </div>
+    </Card>
+  );
 };
 
 export default MenuItemCard;
